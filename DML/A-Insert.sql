a24619320e421ccf051f52ae63bdd92b6152fc28
bjanzen9 <59668573+bjanzen9@users.noreply.github.com>
2020-03-04 15:52:11 -0700

    Update (1 Question Left to Answer)

--- DML/A-Insert.sql
+++ DML/A-Insert.sql
@@ -15,6 +15,12 @@ GO -- Execute the code up to this point as a single batch
     INSERT INTO TableName(Comma, Separated, ListOf, ColumnNames)
     VALUES ('A', 'Value', 'Per', 'Column'),
            ('Another', 'Row', 'Of', 'Values')
+
+	When inserting values, you can use subqueries for individual values
+	provided that the subquery returns a single value:
+
+	INSERT INTO TableName(Comma, Separated, ListOf, ColumnNames)
+	VALUES ('A', (SELECT SingleValue FROM SomeTables), 'Per', 'Column')
     
     Another syntax for the INSERT statement is to use a SELECT clause in place
     of the VALUES clause. This is used for zero-to-many possible rows to insert.
@@ -48,6 +54,14 @@ FROM    Position
 WHERE   PositionID NOT IN (SELECT PositionID FROM Staff)
 --      Add Sheldon Murray as the new Assistant Dean.
 -- TODO: Student Answer Here....
+INSERT INTO Staff(FirstName, LastName, DateHired, PositionID)
+VALUES ('Sheldon', 'Murray', GETDATE(),
+		(SELECT PositionID
+		 FROM	Position
+		 WHERE	PositionDescription = 'Assistant Dean'))
+
+SELECT *
+FROM Staff
 
 -- 3. There are three additional clubs being started at the school:
 --      - START - Small Tech And Research Teams
@@ -63,8 +77,22 @@ VALUES ('START', 'Small Tech And Research Teams'),
 -- 4. In your web browser, use https://randomuser.me/ to get information on three
 --    people to add as new students. Write separate insert statement for each new student.
 -- TODO: Student Answer Here....
+-- TIP: When inserting into a datetime column, you can use a string and SQL Server will convert it for you. E.g. 'Jan 5, 2020'
+
+INSERT INTO Student(FirstName, LastName, Gender, Birthdate)
+VALUES ('Gladys', 'Phillips', 'F', 'June 7, 1956')
+
+INSERT INTO Student(FirstName, LastName, Gender, Birthdate)
+VALUES ('Suzanne', 'Harris', 'F', 'Aug 1, 1989')
+
+INSERT INTO Student(FirstName, LastName, Gender, Birthdate)
+VALUES ('Pauline', 'Perkins', 'F', 'July 7 1979')
+
 
 
 -- 5. Enroll each of the students you've added into the DMIT777 course.
 --    Use 'Dan Gilleland' as the instructor. At this point, their marks should be NULL.
 -- TODO: Student Answer Here....
+-- SP_HELP Student -- This will show all the details for each component of the table
+
+
